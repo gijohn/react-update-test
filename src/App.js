3eec618a51c48a06d791a508c8f4542fc2bc922a
Giftson John <gjohn@redhat.com>
2019-11-06 17:39:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Update #7.
+          Edit <code>src/App.js</code> and save to reload. Update #8.
         </p>
         <a
           className="App-link"
